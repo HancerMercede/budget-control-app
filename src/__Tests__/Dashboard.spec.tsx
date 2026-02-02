8a4385781527261cb6549ec7cdd4e12bb3cc57e7
Hancer <hancer.mercede@arroyoconsulting.net>
2026-02-02 14:12:28 -0400

    adding new test to the app, and adding husky

---
+++ src/__Tests__/Dashboard.spec.tsx
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+import { DashboardPage } from "../pages/DashboardPage";
+import { render } from "@testing-library/react";
+import { getRemoteBudget } from "../config/firebase";
+
+describe("Dashboard", () => {
+  test("Render the dashboard page", () => {
+    render(<DashboardPage />);
+  });
+});
+
+describe("Get Budget", () => {
+  test("Budget must be 50,000", async () => {
+    const userId = "jDADi4kIuYe3rwxJrkamgmHVSk23";
+    const budget = await getRemoteBudget(userId);
+    console.log(budget);
+
+    expect(budget).toBe(50000);
+  });
+});
